dd1fbc5d1f1a483edb56bfa2fb029835f698249e
mihacuo <mihhail.odnolko@gmail.com>
2023-01-13 19:34:41 +0000

    highscorees redirection works

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,19 @@
 console.log('Loaded logic.js');
 
 // set length of the quiz in seconds
-quizTime = 3;
+quizTime = 20;
 // set length of penalty in seconds
 quizPenaltyTime = 5;
 
 // We get all the required elements in this section
 var startButton = document.getElementById("start")
 var timeLeft = document.getElementById("time");
+var startScreen = document.getElementById("start-screen")
+var questionTitle = document.getElementById("question-title");
+var quesionSection = document.getElementById("questions");
+var multipleAnswers = document.getElementById("choices")
+
+console.log(questionTitle)
 
 // Let set default Time display
 timeLeft.textContent = quizTime;
@@ -18,16 +24,18 @@ function startQuiz(event) {
   timeLeft.textContent = quizTime;
   if (quizTime === 0) {
     clearInterval(quizTimer);
+    window.location.href = "highscores.html"
   }
 
 }
 
-
-
 startButton.addEventListener("click", function(event) {
   event.preventDefault();
   quizTimer = setInterval(startQuiz, 1000);
-  window.location.href("highscores.html")
-
-
-})
+  startScreen.setAttribute("class", "hide");
+  quesionSection.setAttribute("class", "block");
+  for (var i = 0; i < questionsBank.length; i++) {
+    console.log(questionsBank[i]["question"])
+    questionTitle.textContent = questionsBank[i]["question"]
+  }
+});
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,24 @@
 console.log('Loaded questions.js');
+
+questionsBank = [
+  {
+    question: "How do you declare a variable in JavaScipt",
+    answers: [
+      'var',
+      'let', 
+      'const',
+      'all of the above'
+    ],
+    answerIndex: 3
+  },
+  {
+    question: "Which one is true?",
+    answers: [
+      'You need to learn Java before Javascript',
+      'Javascipt is typically run on the server-side machine',
+      'JavaScript has a unique position as the most widely-adopted browser language, fully integrated with HTML/CSS',
+      'JavaScript is not as powerful as other languages like Java, Python etc.'
+    ],
+    answerIndex: 2
+  }
+]
