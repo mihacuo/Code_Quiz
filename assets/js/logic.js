3aead6230753a26d6bd3ff021949ce78931b5788
mihacuo <mihhail.odnolko@gmail.com>
2023-01-14 16:47:33 +0000

    work on questions section

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,17 @@
-console.log('Loaded logic.js');
+console.log("Loaded logic.js");
 
 // set length of the quiz in seconds
-quizTime = 20;
+quizTime = 205;
 // set length of penalty in seconds
 quizPenaltyTime = 5;
 
 // We get all the required elements in this section
-var startButton = document.getElementById("start")
+var startButton = document.getElementById("start");
 var timeLeft = document.getElementById("time");
-var startScreen = document.getElementById("start-screen")
+var startScreen = document.getElementById("start-screen");
 var questionTitle = document.getElementById("question-title");
-var quesionSection = document.getElementById("questions");
-var multipleAnswers = document.getElementById("choices")
-
-console.log(questionTitle)
+var questionSection = document.getElementById("questions");
+var multipleAnswers = document.getElementById("choices");
 
 // Let set default Time display
 timeLeft.textContent = quizTime;
@@ -24,18 +22,26 @@ function startQuiz(event) {
   timeLeft.textContent = quizTime;
   if (quizTime === 0) {
     clearInterval(quizTimer);
-    window.location.href = "highscores.html"
+    window.location.href = "highscores.html";
   }
-
 }
 
-startButton.addEventListener("click", function(event) {
+startButton.addEventListener("click", function (event) {
   event.preventDefault();
   quizTimer = setInterval(startQuiz, 1000);
   startScreen.setAttribute("class", "hide");
-  quesionSection.setAttribute("class", "block");
-  for (var i = 0; i < questionsBank.length; i++) {
-    console.log(questionsBank[i]["question"])
-    questionTitle.textContent = questionsBank[i]["question"]
+  questionSection.setAttribute("class", "");
+  i= 0 ;
+  console.log(questionsBank[i]["question"]);
+  questionTitle.textContent = questionsBank[i]["question"];
+  var answersSection = document.createElement("ol");
+  for (j = 0; j < questionsBank[i]["answers"].length; j++) {
+    console.log(j);
+    var answerTAG = document.createElement("li");
+    console.log(questionsBank[i]["answers"][j]);
+    answerTAG.textContent = questionsBank[i]["answers"][j];
+    answerTAG.setAttribute("data-chosen-answer", j);
+    answersSection.appendChild(answerTAG);
   }
+  multipleAnswers.appendChild(answersSection)
 });
